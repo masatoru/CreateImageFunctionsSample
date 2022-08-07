13e0975a58c57b1c217f35cc26cb9c3452256bd6
Satoru Fujimori <satoru.fujimori@gmail.com>
2022-08-07 15:13:36 +0900

    Added font name for WSL

--- ImageSharpSample .cs
+++ ImageSharpSample .cs
@@ -27,26 +27,22 @@ namespace Company.Function
         {
             Image img = new Image<Rgba32>(500, 200);
             Font fnt = SystemFonts.CreateFont("MS UI Gothic", 50);
+            // Font fnt = SystemFonts.CreateFont("IPAexMincho", 50);
 
             TextOptions options = new(fnt)
             {
                 Origin = new SixLabors.ImageSharp.PointF(10, 50), // Set the rendering origin.
-                // TabWidth = 8, // A tab renders as 8 spaces wide
-                // WrappingLength = 100, // Greater than zero so we will word wrap at 100 pixels wide
                 HorizontalAlignment = HorizontalAlignment.Left
             };
 
-            PathBuilder pathBuilder = new PathBuilder();
+            PathBuilder pathBuilder = new();
             pathBuilder.SetOrigin(new PointF(0, 0));
             pathBuilder.AddLines(new PointF(0, 0), new PointF(500, 0), new PointF(500, 200), new PointF(0, 200), new PointF(0, 0));
-            // add more complex paths and shapes here.
-
-            IPath path = pathBuilder.Build();
+            var path = pathBuilder.Build();
 
             IBrush brush = Brushes.Horizontal(Color.Blue, Color.Blue);
             IPen pen = SixLabors.ImageSharp.Drawing.Processing.Pens.Solid(Color.Blue, 1);
 
-            // IBrush brush = Brushes.Horizontal(Color.DarkOrange, Color.White);
             img.Mutate(ctx => ctx
                 .DrawText(options, "こんにちは .NET6", brush, pen)
                 .Draw(Color.DarkOrange, 5, path));
